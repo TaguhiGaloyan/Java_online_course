2e1a01b65b3a8de9690b120142feec5e5d560019
unknown <Taguhi Galoyan>
2022-10-23 01:57:07 +0400

    My first git commit

---
+++ Lesson1/Lesson1/src/main/java/Lesson1.java
@@ -0,0 +1,17 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Barev");
+        System.out.println("Lesson1");
+
+        short y = 452;
+        System.out.println(y);
+
+        long b = 54652652651;
+        System.out.println(b);
+
+
+
+
+
+    }
+}
